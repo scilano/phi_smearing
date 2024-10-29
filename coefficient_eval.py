7fdd4fc86cf8ea6efa45bb2d486a6e8951971520
Nicolas Crépet <scilano@live.fr>
2024-10-29 14:02:44 +0100

    Using numba to speed up computation

--- coefficient_eval.py
+++ coefficient_eval.py
@@ -12,30 +12,33 @@ import tqdm
 import os
 import mpmath as mp
 from ast import literal_eval
+from numba import jit, njit, vectorize, float64, int8, cfunc
 
 path = os.path.dirname(os.path.abspath(__file__)) + "/"
 config = configparser.ConfigParser()
 config.read(path + "config.ini")
 ion = config.get("General", "ion_name")
 
+N_terms = config.getint("formfactor", "N_terms")
+WoodsSaxon = literal_eval(config.get("Ion", "WoodsSaxon"))
+R = WoodsSaxon[ion][0] / 0.197
+a = WoodsSaxon[ion][1] / 0.197
+Z = WoodsSaxon[ion][3]
+if Z > 1:
+    rho0 = -1 / (8 * np.pi * a**3 * mp.polylog(3, -np.exp(R / a)))
+else:
+    rho0 = 1
 
+
+@njit()
 def formfactor(k, RA, aA, Z):
     if Z > 1:
         if k * np.pi * aA < 250:
-            rho0 = -1 / (8 * np.pi * aA**3 * mp.polylog(3, -np.exp(RA / aA)))
-            N_terms = config.getint("formfactor", "N_terms")
-
             Fch1 = (
-                4
-                * np.pi**2
-                * rho0
-                * aA**3
-                / (k**2 * aA**2 * np.sinh(np.pi * k * aA) ** 2)
-                * (np.pi * k * aA * np.cosh(np.pi * k * aA) * np.sin(k * RA) - k * RA * np.sinh(np.pi * k * aA) * np.cos(k * RA))
+                4 * np.pi**2 * aA**3 / (k**2 * aA**2 * np.sinh(np.pi * k * aA) ** 2) * (np.pi * k * aA * np.cosh(np.pi * k * aA) * np.sin(k * RA) - k * RA * np.sinh(np.pi * k * aA) * np.cos(k * RA))
             )
-            Fch2_term = [(-1 if i % 2 == 0 else 1) * i * np.exp(-i * RA / aA) / (i**2 + k**2 * aA**2) for i in range(1, N_terms + 1)]
-
-            return float(Fch1 + 8 * np.pi * rho0 * aA**3 * np.sum(Fch2_term))
+            Fch2_term = np.array([(-1 if i % 2 == 0 else 1) * i * np.exp(-i * RA / aA) / (i**2 + k**2 * aA**2) for i in range(1, N_terms + 1)])
+            return float(Fch1 + 8 * np.pi * aA**3 * np.sum(Fch2_term))
         else:
             return 0
     else:
@@ -59,13 +62,30 @@ def formfactor(k, RA, aA, Z):
             return (a0 + a1 * k2 + a2 * k2**2 + a3 * k2**3 + a4 * k2**4) / (1 + aa**2 * k2) ** 2
 
 
+@njit()
 def photon_flux(x, k2, RA, aA, Z):
-    alphaem = 1 / 137.035999084
     mproton = 0.9382720813
     Q2 = k2 + x**2 * mproton**2
-    return Z**2 * alphaem / (np.pi**2) * k2 * (formfactor(np.sqrt(Q2), RA, aA, Z) / Q2) ** 2
+    return k2 * (formfactor(np.sqrt(Q2), RA, aA, Z) / Q2) ** 2
 
 
+def integrate(X, RA, aA, Z, f):
+    x1, x2, qt = X
+    bornsup = 3 * np.log(10)
+    alphaem = 1 / 137.035999084
+
+    def func(lnpt1, theta):
+        pt1 = np.exp(lnpt1)
+        return pt1**2 * f(x1, x2, pt1, qt, theta, RA, aA, Z)
+
+    result, err = scp.integrate.dblquad(func, 0, 2 * np.pi, -np.inf, bornsup)
+    if err / result > 10:
+        print("err>result*10. Try increase tol")
+        result, err = scp.integrate.dblquad(func, 0, 2 * np.pi, -np.inf, bornsup, epsabs=1e-10)
+    return alphaem**2 * (Z * rho0 / np.pi) ** 4 * result
+
+
+@njit()
 def I1_integrand(x1, x2, pt1, qt, th, RA, aA, Z):
     pt12 = pt1 * pt1
     qt2 = qt * qt
@@ -73,15 +93,9 @@ def I1_integrand(x1, x2, pt1, qt, th, RA, aA, Z):
     return photon_flux(x1, pt12, RA, aA, Z) * photon_flux(x2, pt22, RA, aA, Z)
 
 
-def I1_eval(X, RA, aA, Z, bornsup):
-    x1, x2, qt = X
-
-    def func(lnpt1, theta):
-        pt1 = np.exp(lnpt1)
-        return pt1**2 * I1_integrand(x1, x2, pt1, qt, theta, RA, aA, Z)
-
-    result = scp.integrate.dblquad(func, 0, 2 * np.pi, -np.inf, bornsup)
-    return result[0]
+def I1_eval(X, RA, aA, Z):
+    result = integrate(X, RA, aA, Z, I1_integrand)
+    return result
 
 
 def I1_eval_old(X, RA, aA, Z):
@@ -111,6 +125,7 @@ def I1_eval_old(X, RA, aA, Z):
     return res
 
 
+@njit()
 def I2_integrand(x1, x2, pt1, qt, th, RA, aA, Z):
     pt12 = pt1 * pt1
     qt2 = qt * qt
@@ -123,17 +138,12 @@ def I2_integrand(x1, x2, pt1, qt, th, RA, aA, Z):
     return (2 * A - 1) * N
 
 
-def I2_eval(X, RA, aA, Z, bornsup):
-    x1, x2, qt = X
-
-    def func(lnpt1, theta):
-        pt1 = np.exp(lnpt1)
-        return pt1**2 * I2_integrand(x1, x2, pt1, qt, theta, RA, aA, Z)
-
-    result = scp.integrate.dblquad(func, 0, 2 * np.pi, -np.inf, bornsup)
-    return result[0]
+def I2_eval(X, RA, aA, Z):
+    result = integrate(X, RA, aA, Z, I2_integrand)
+    return result
 
 
+@njit()
 def I3_integrand(x1, x2, pt1, qt, th, RA, aA, Z):
     qt2 = qt * qt
     pt12 = pt1 * pt1
@@ -149,17 +159,12 @@ def I3_integrand(x1, x2, pt1, qt, th, RA, aA, Z):
     return (2 * (A + B) - 2) * N
 
 
-def I3_eval(X, RA, aA, Z, bornsup):
-    x1, x2, qt = X
-
-    def func(lnpt1, theta):
-        pt1 = np.exp(lnpt1)
-        return pt1**2 * I3_integrand(x1, x2, pt1, qt, theta, RA, aA, Z)
-
-    result = scp.integrate.dblquad(func, 0, 2 * np.pi, -np.inf, bornsup)
-    return result[0]
+def I3_eval(X, RA, aA, Z):
+    result = integrate(X, RA, aA, Z, I3_integrand)
+    return result
 
 
+@njit()
 def I4_integrand(x1, x2, pt1, qt, th, RA, aA, Z):
     qt2 = qt * qt
     pt12 = pt1 * pt1
@@ -178,15 +183,34 @@ def I4_integrand(x1, x2, pt1, qt, th, RA, aA, Z):
     return (2 * (A - B) ** 2 - 1) * N
 
 
-def I4_eval(X, RA, aA, Z, bornsup):
-    x1, x2, qt = X
+def I4_eval(X, RA, aA, Z):
+    result = integrate(X, RA, aA, Z, I4_integrand)
+    return result
 
-    def func(lnpt1, theta):
-        pt1 = np.exp(lnpt1)
-        return pt1**2 * I4_integrand(x1, x2, pt1, qt, theta, RA, aA, Z)
 
-    result = scp.integrate.dblquad(func, 0, 2 * np.pi, -np.inf, bornsup)
-    return result[0]
+def grid_peak_integration(grid, plot=False):
+    X = grid.axis
+    Y = grid.values
+    # Detect peaks
+    peaks = signal.find_peaks(-Y, distance=1)[0]
+    peaks = np.append([0], peaks)
+    peaks = np.append(peaks, [len(X) - 1])
+    if plot:
+        plt.plot(X, Y)
+        plt.plot(X[peaks], Y[peaks], marker="x", linestyle="None")
+        plt.semilogx()
+        plt.semilogy()
+        plt.show()
+    I = 0
+    e = 0
+    # Integrate between each peak
+    for i in range(len(peaks) - 1):
+        p1 = peaks[i]
+        p2 = peaks[i + 1]
+        f = lambda x: grid.evaluate(x)
+        res = scp.integrate.quad(f, X[p1], X[p2])
+        I += res[0]
+    return I
 
 
 def main():
--- test.py
+++ test.py
@@ -1,16 +1,54 @@
+from numba import njit
 from time import perf_counter
-import coefficient_eval
 import numpy as np
+import coefficient_eval as c_eval
+import tqdm
+from ast import literal_eval
+import configparser
 
-t1 = perf_counter()
-a = coefficient_eval.I1_eval([1e-2, 1e-2, 1e-2], 1, 1, 1, np.log(10) * 3)
-t2 = perf_counter()
-b = coefficient_eval.I1_eval([1e-2, 1e-2, 1e-2], 1, 1, 1, 5)
-t3 = perf_counter()
-c = coefficient_eval.I1_eval_old([1e-2, 1e-2, 1e-2], 1, 1, 1)
-t4 = perf_counter()
-print("method1")
-print(t2 - t1)
-print("method2")
-print(t4 - t3)
-print(a, b, c)
+config = configparser.ConfigParser()
+config.read("config.ini")
+path = config.get("General", "path")
+ion = config.get("General", "ion_name")
+WoodsSaxon = literal_eval(config.get("Ion", "WoodsSaxon"))
+R = WoodsSaxon[ion][0] / 0.197
+a = WoodsSaxon[ion][1] / 0.197
+Z = WoodsSaxon[ion][3]
+
+Npointsx1 = config.getint("grid", "Npointsx1")
+Npointsx2 = config.getint("grid", "Npointsx2")
+Npointsqt = config.getint("grid", "Npointsqt")
+logx1min = config.getfloat("grid", "logx1min")
+logx1max = config.getfloat("grid", "logx1max")
+logx2min = config.getfloat("grid", "logx2min")
+logx2max = config.getfloat("grid", "logx2max")
+logqtmin = config.getfloat("grid", "logqtmin")
+logqtmax = config.getfloat("grid", "logqtmax")
+
+X1 = np.logspace(logx1min, logx1max, Npointsx1)
+X2 = np.logspace(logx2min, logx2max, Npointsx2)
+QT = np.logspace(logqtmin, logqtmax, Npointsqt)
+
+
+# t1 = perf_counter()
+# I = c_eval.I1_eval([QT[1], X1[1], X2[1]], R, a, Z)
+# t2 = perf_counter()
+# print("new method")
+# print(I)
+# print(t2 - t1)
+# print(ion)
+
+
+def main():
+    integratetime = np.zeros_like(QT)
+    for i, q in enumerate(QT):
+        # t1 = perf_counter()
+        I = c_eval.I3_eval([q, X1[-1], X2[-1]], R, a, Z)
+        # t2 = perf_counter()
+        # integratetime[i] = t2 - t1
+        print(q, I)
+    # print(f"avg time: {np.mean(integratetime)}")
+
+
+if __name__ == "__main__":
+    main()
